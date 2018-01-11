3c8127e930193751041986db854bbe0c90695eca
Gaurav Garg <garg.gaurav.1989it@gmail.com>
2018-01-11 17:34:12 +0530

    added async validator, custom validator and formArray

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { ActivatedRoute, Params } from '@angular/router';
 import { Observable, Observer, Subscription } from 'rxjs/Rx';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -12,18 +12,25 @@ export class HomeComponent implements OnInit {
 
   signupForm: FormGroup;
 
+  existingUserNames: string[] = ['Lara', 'Java'];
+  
   constructor() {
 
     this.signupForm = new FormGroup({
 
       'userDataForm': new FormGroup({
-        'username' : new FormControl(null, [Validators.required]),
+        'username' : new FormControl(null, [Validators.required, this.validateUserNames.bind(this)]),
         'email': new FormControl("a@a.com", [Validators.required, 
-          Validators.email])
+          Validators.email], [this.validateEmailId])
       }),
-       'address': new FormControl(null)
+       'address': new FormControl(null),
+       'fields': new FormArray([])
     })
 
+   }
+
+   addField(){
+     (<FormArray>this.signupForm.get("fields")).push(new FormControl())
    }
 
    submitForm(){
@@ -32,6 +39,41 @@ export class HomeComponent implements OnInit {
    }
 
   ngOnInit() {
+
+  }
+
+
+
+  validateUserNames(control: FormControl) : {[s: string]: boolean}{
+
+    if(this.existingUserNames.indexOf(control.value) != -1 ){
+      return {'nameExist': true}
+    }
+    return null;
+  }
+
+  validateEmailId(control: FormControl): Observable<any> | Promise<any>{
+    const emailPromise = new Promise<any>(
+      (resolve, reject) => {
+        setTimeout(
+          () => {
+
+            if(control.value == "b@b.com"){
+              resolve({'validEmail': true});
+            }else{
+              resolve(null);
+            }
+          }, 5000
+        )
+      }
+    )
+
+    return emailPromise;
+  }
+
+  reset(){
+
+    this.signupForm.reset();
 
   }
 }
